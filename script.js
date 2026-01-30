8356cc099456c6aef87b1f4567b0b5ce533bad2f
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2026-01-30 21:46:00 +0530

    To Do list Logic added

--- script.js
+++ script.js
@@ -17,3 +17,69 @@ function openCard() {
   });
 }
 openCard()
+
+function toDoList() {
+  // Task Data----------------------------------------------
+
+  var currentTask = [];
+  //with quotes means key and without quote is variable of a value
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("Task list is empty");
+  }
+
+  // Showing task in Div Function --------------------------------
+  function renderTask() {
+    var allTask = document.querySelector(".allTask");
+    var sum = "";
+    //foreach for display tasks
+    currentTask.forEach(function (elem, idx) {
+      // console.log(elem.details);
+      sum =
+        sum +
+        `<div class="task">
+    <h5>${elem.task}<span class=${elem.imp}>imp</span></h5>
+    
+    <button id = ${idx}>Mark as Complete</button>
+    </div>`;
+    });
+
+    allTask.innerHTML = sum;
+
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    document.querySelectorAll(".task button").forEach(function (btn) {
+        btn.addEventListener("click", function () {
+          currentTask.splice(btn.id, 1);
+
+          renderTask();
+        });
+      });
+  }
+
+  // --------------------------------------------------------------------
+
+  let form = document.querySelector(".addTask form ");
+  let taskInput = document.querySelector(".addTask form #task-input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  // Submit Button pressed function-----------------------------------
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked,
+    });
+    renderTask();
+
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+    taskCheckbox.checked = "";
+  
+  });
+}
+toDoList()
